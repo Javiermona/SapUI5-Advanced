172c23be469e5ccaff752c4f1cb1faa39dc23045
Javiermona <javiermona@gmail.com>
2021-12-27 18:52:54 +0000

    first commit 04

--- employees/webapp/controller/MainView.controller.js
+++ employees/webapp/controller/MainView.controller.js
@@ -1,15 +1,27 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
+sap.ui.define(["sap/ui/core/mvc/Controller"],
+/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
     function (Controller) {
-        "use strict";
+    "use strict";
 
-        return Controller.extend("logaligroup.employees.controller.MainView", {
-            onInit: function () {
+   var Main = Controller.extend("logaligroup.employees.controller.MainView", {});
+      
 
+        Main.prototype.onValidate =   function() {
+            let inputEmployee = this.byId("inputEmployee");
+            let valueEmployee = inputEmployee.getValue();
+
+            if (valueEmployee.length === 6) {
+               // inputEmployee.setDescription("OK");
+               this.getView().byId("labelCountry").setVisible(true);
+               this.getView().byId("slCountry").setVisible(true);
+            } else {
+               //  inputEmployee.setDescription("NOT OK");
+               this.getView().byId("labelCountry").setVisible(false);
+               this.getView().byId("slCountry").setVisible(false);
             }
-        });
-    });
+        };
+        return Main;
+    
+});
