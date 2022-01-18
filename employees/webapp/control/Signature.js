46514695398d7920da338a2e5e6e76ff84de05c4
Javiermona <javiermona@gmail.com>
2022-01-18 22:45:29 +0000

    odataimagen

--- employees/webapp/control/Signature.js
+++ employees/webapp/control/Signature.js
@@ -1,61 +1,75 @@
-sap.ui.define([
-    "sap/ui/core/Control"
-], function (Control) {
-        "use strict";
+sap.ui.define(["sap/ui/core/Control"], function (Control) {
+    "use strict";
 
     return Control.extend("logaligroup.employees.control.Signature", {
         metadata: {
-        properties: {
-        "width": {
-        type: "sap.ui.core.CSSSize",
-        defaultValue: "400px"
-        },
-        "height": {
-        type: "sap.ui.core.CSSSize",
-        defaultValue: "100px"
-        },
-        "bgcolor": {
-        type: "sap.ui.core.CSSColor",
-        defaultValue: "white"
-                      }
+            properties: {
+                "width": {
+                    type: "sap.ui.core.CSSSize",
+                    defaultValue: "400px"
+                },
+                "height": {
+                    type: "sap.ui.core.CSSSize",
+                    defaultValue: "100px"
+                },
+                "bgcolor": {
+                    type: "sap.ui.core.CSSColor",
+                    defaultValue: "white"
+                }
             }
-        }   ,     
-
-        onInit: function () {
-
-            
         },
+
+        onInit: function () {},
         renderer: function (oRm, oControl) {
             oRm.write("<div");
-            oRm.addStyle("width", oControl.getProperty("width"));
-            oRm.addStyle("height", oControl.getProperty("height"));
-            
-            oRm.addStyle("background-color", oControl.getProperty("bgcolor"));
-            
-            oRm.addStyle("border", "1px solid black");
-            oRm.writeStyles();
-            oRm.write(">");
-            oRm.write("<canvas width='" + oControl.getProperty("width") +
-            "' " +
-                "height='" + oControl.getProperty("height") + "'");
-            oRm.write("></canvas>");
-            oRm.write("</div>");
-            },
-            onAfterRendering: function () {
-            var canvas = document.querySelector("canvas");
-            try {
-            // @ts-ignore
-            this.signaturePad = new SignaturePad(canvas);
-            } catch (e) {
-            console.error(e);
-            }
-            },
-            clear: function () {
-            this.signaturePad.clear();
-            }
+                    oRm.addStyle("width", oControl.getProperty("width"));
+                    oRm.addStyle("height", oControl.getProperty("height"));
+                    oRm.addStyle("background-color", oControl.getProperty("bgcolor"));
+                    oRm.addStyle("border", "1px solid black");
+                    oRm.writeStyles();
+                    oRm.write(">");
+                    oRm.write("<canvas width='" + oControl.getProperty("width") + "' " + "height='" + oControl.getProperty("height") + "'");
+                    oRm.write("></canvas>");
+                    oRm.write("</div>");
+        },
 
-       
+        onAfterRendering: function () {
+            var canvas = document.querySelector("canvas");
+        try {
+            // @ts-ignore
+                this.signaturePad = new SignaturePad(canvas);
+                this.signaturePad.fill = false;
+                canvas.addEventListener("mousedown", function(){
+                    this.signaturePad.fill = true;
+
+                }.bind(this));
+
+            } catch (e) {
+                console.error(e);
+            }
+        },
+        clear: function () {
+            this.signaturePad.clear();
+            this.signaturePad.fill = false;
+        },
+
+        isFull: function () {
+        return this.signaturePad.fill;
+
+        },
+        getSignature: function() {
+            return this.signaturePad.toDataURL();
+
+        },
+
+        setSignature: function(signature){
+
+      this.signaturePad.fromDataURL(signature);
+
+        }
+
+
 
     });
-   
+
 });
--- employees/webapp/controller/OrderDetails.controller.js
+++ employees/webapp/controller/OrderDetails.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"
+    "sap/ui/core/mvc/Controller", 
+    "sap/ui/core/routing/History",
+    "sap/m/MessageBox"
 
-], function (Controller, History) {
+], function (Controller, History, MessageBox) {
 
     function _onObjectMatched(oEvent){
         this.getView().bindElement({    
@@ -38,6 +40,75 @@ sap.ui.define([
                 signature.clear();
 
 
+        },
+        onSaveSignatre: function(oEvent){
+        //obtengo instacia de la firma
+            const signature = this.byId("signature");
+            const oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+            let signaturePng;
+
+            if(!signature.isFull()) {//Si no tenemos la firma
+                MessageBox.error(oResourceBundle.getText("fillSignature"));
+            }else {
+
+                signaturePng = signature.getSignature().replace("data:image/png;base64","");
+
+            };
+
+            let objectOrder = oEvent.getSource().getBindingContext("odataNorthwind").getObject(); //Obbtengi el id
+            let body = {
+                            orderID : objectOrder.OrderID.toString(),
+                            sapId: this.getOwnerComponent().SapId,
+                            employeeId: objectOrder.EmployeeID.toString(),
+                            MimeType : "image/png",
+                            MediaContent: signaturePng  //Contenido en binario
+            };
+
+            this.getView().getModel("incidenceModel").create("/SignatureSet", body, {
+                success: function(){ 
+                        MessageBox.information(oResourceBundle.getText("SignatureSave"));                          
+                },
+                error: function(){ 
+                     MessageBox.error(oResourceBundle.getText("SignatureError"));
+            },
+
+            });
+
+
+
+
+        },
+
+        factoryOrderDetails: function(listId, oContext) {
+            let contextObject = oContext.getObject();
+            contextObject.Currency = "EUR";
+
+            let UnitsInStock = oContext.getModel().getProperty("/Products(" + contextObject.ProductID + ")/UnitsInStock" ); //unidades en stock
+           // odataNorthwind>Order_Detail
+            if ( contextObject.Quantity <= UnitsInStock){
+                let objectListItem = new sap.m.ObjectListItem({
+                    title: "{odataNorthwind>/Products(" + contextObject.ProductID + ")/ProductName} ({odataNorthwind>Quantity})",
+                    number: "{parts: [{ path: 'odataNorthwind>UnitPrice'}, { path: 'odataNorthwind>Currency'}], type: 'sap.ui.model.type.Currency', formatOptions:{showMeasure: false}}",
+                    numberUnit: "{odataNorthwind>Currency}"
+
+                });
+                return objectListItem;
+
+            }else{
+                let CustomListItem = new sap.m.CustomListItem({
+                        content: [
+                                new sap.m.Bar({
+                                    contentLeft:   new sap.m.Label({text: "{odataNorthwind>/Products(" + contextObject.ProductID + ")/ProductName} ({odataNorthwind>Quantity})"}) ,
+                                    contentMiddle: new sap.m.ObjectStatus({ text: "{i18n>AvailabelStock} {odataNorthwind>/Prodcuts(" + contextObject.ProductID + ")/UnitsInStock}", state: "Error" }),
+                                    contentRight:  new sap.m.Label({text: "{parts: [ { path: 'odataNorthwind>UnitPrice'}, { path: 'odataNorthwind>Currency'}], type: 'sap.ui.model.type.Currency'}"})
+                                })
+
+                        ]
+
+                });
+                return CustomListItem;
+
+            }
         }
 
     });
